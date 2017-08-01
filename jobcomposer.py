fcccd962cae065b31480f3d6ec5ae292929226cd
Roman Reimche <roman.reimche@gmail.com>
2017-08-01 20:26:27 +0200

    Simplified input and output formats for jobcomposer.py

--- jobcomposer.py
+++ jobcomposer.py
@@ -1,12 +1,11 @@
-# text file with headlines must be of format:
-# [headline in plain text][one space[nickname/id for this headline]
+# creates jobs to scrap twitter with twitterscrapper for the timeframe since 2016-01-01 until 2016-12-31
 
 import argparse
-import csv
+#import csv
 
-from requests.utils import quote
+#from requests.utils import quote
 
-parser = argparse.ArgumentParser("Scrapper")
+parser = argparse.ArgumentParser("jobcomposer")
 parser.add_argument("inputfile", help="Filename to parse for headlines", type=str)
 parser.add_argument("outputfile", help="Filename to parse for headlines", type=str)
 parser.add_argument("database", help="Database to save to", type=str)
@@ -16,14 +15,22 @@ args = parser.parse_args()
 headlines = open(args.inputfile,"r")
 jobs = open(args.outputfile,"w")
 
-rows = csv.reader(headlines, delimiter=" ", quotechar="|")
-for row in rows:
-    news_id = row[1]
-    encoded_query = quote(row[0])
-    encoded_query += "%20since%3A2016-01-01%20until%3A2016-12-31"
+count = 0
+for line in headlines.read().split("\n"):
     for i in range(3):
-        job = "python scrapper.py " + args.database + " " + news_id + str(i) + " " + encoded_query + " " + str(args.limit) + "\n"
+        encoded_query = line + "%20since%3A2016-01-01%20until%3A2016-12-31"
+        job = "python scrapper.py " + args.database + " " + str(count) + "_" + str(i) + " " + encoded_query + " " + str(args.limit) + "\n"
         jobs.write(job)
+    count += 1
+
+# rows = csv.reader(headlines, delimiter=" ", quotechar="|")
+# for row in rows:
+#     news_id = row[1]
+#     encoded_query = quote(row[0])
+#     encoded_query += "%20since%3A2016-01-01%20until%3A2016-12-31"
+#     for i in range(3):
+#         job = "python scrapper.py " + args.database + " " + news_id + str(i) + " " + encoded_query + " " + str(args.limit) + "\n"
+#         jobs.write(job)
 
 headlines.close()
 jobs.close()
